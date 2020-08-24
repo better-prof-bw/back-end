4afdab85c1bb3f56d4c43febae3e80f29878e8d7
emilme <emilme@sir.mle>
2020-08-24 12:59:54 -0700

    automatic deployment test 2

--- api/server.js
+++ api/server.js
@@ -9,7 +9,7 @@ const server = express();
  
 server.use(helmet());
 server.use(express.json());
-server.use(cors());
+server.use(cors())
 
 server.use("/auth", authRouter);
 server.use("/users", usersRouter);
